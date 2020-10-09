a7c6e1f09610222435f8bd87b3d13cd3e9311f8b
rohanJa <rohan.jain150300@gmail.com>
2020-10-09 22:26:21 +0530

    added search in list according to category through url

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,9 +1,28 @@
+from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView
+from django.views.generic.list import ListView
+from .models import RestaurantLocation
 
 # Create your views here.
-# function based views
 
-def home(request):
+class RestaurantListView(ListView):
+    queryset=RestaurantLocation.objects.all()
+    template_name='restaurants/restaurants_list.html'
 
-    return render(request,"base.html", {"html_var":"AJJU"})
+class SearchRestaurantListView(ListView):
+    template_name='restaurants/restaurants_list.html'
+    
+    def get_queryset(self):
+        slug=self.kwargs.get('slug')
+        if slug:
+            # search for exact word or contains "substring" of the category not "subsequence" in the Database
+            queryset = RestaurantLocation.objects.filter(
+                    Q(category__iexact=slug) | 
+                    Q(category__icontains=slug)
+                )
+        else:
+            queryset=RestaurantLocation.objects.none()
+        return queryset
