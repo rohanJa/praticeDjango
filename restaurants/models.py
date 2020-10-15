549d19fc3d8561ed745307f55ff76184fb006867
rohanJa <rohan.jain150300@gmail.com>
2020-10-15 23:21:50 +0530

    Adding custom validation in models

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.db.models.signals import pre_save, post_save
-from .utils import unique_slug_generator
 
+from .utils import unique_slug_generator
+from .validators import validate_category
 # Create your models here.
 class RestaurantLocation(models.Model):
     name = models.CharField(max_length=120)
     location = models.CharField(max_length=120, null=True, blank=True)
-    category = models.CharField(max_length=120, null=True, blank=True)
+    category = models.CharField(max_length=120, null=True, blank=True, validators=[validate_category])
     timestamp = models.DateTimeField(auto_now_add=True) 
     updated = models.DateTimeField(auto_now=True) 
     slug = models.SlugField(null=True, blank=True)
@@ -22,6 +23,7 @@ class RestaurantLocation(models.Model):
 # Signals in Model
 
 def rl_pre_save_receiver(sender, instance, *args, **kwargs):
+    instance.category = instance.category.capitalize()
     if not instance.slug:
         instance.slug = unique_slug_generator(instance)
 
